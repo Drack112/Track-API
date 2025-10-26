9ad842aa6e59dec068c885349bbf6a4ab5407d05
Drack112 <onepessoa3@gmail.com>
2025-10-26 15:10:27 -0300

    feat: basic setup fo the main file with the packages setting up

--- cmd/track-api/main.go
+++ cmd/track-api/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Drack112/Track-API/internal/adapter/secondary/contextlogger"
+	"github.com/Drack112/Track-API/internal/adapter/secondary/crypto"
+	"github.com/Drack112/Track-API/internal/platform/config"
+	"github.com/Drack112/Track-API/internal/platform/ports/output/keygen"
+	"github.com/Drack112/Track-API/internal/platform/ports/output/logger"
+	"github.com/Drack112/Track-API/internal/shared/constants/commonkeys"
 )
 
 const (
@@ -23,7 +31,36 @@ func run() int {
 	logs, cleanupLogger := contextlogger.New()
 	defer cleanupLogger()
 
-	logs.Infof("Running")
+	keyGenerator := crypto.New()
 
-	return 1
+	_, err := loadConfig(keyGenerator, logs)
+	if err != nil {
+		logs.Errorw(ErrLoadConfig, commonkeys.Error, err.Error())
+		return 2
+	}
+
+	_, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	return 0
+}
+
+func loadConfig(keyGenerator keygen.Generator, logs logger.ContextLogger) (*config.Config, error) {
+	cfg, err := config.New(keyGenerator).Load(logs)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+
+	logs.Infow(
+		MsgConfigLoaded,
+		commonkeys.APIName, cfg.General.Name,
+		commonkeys.AppEnv, cfg.General.Env,
+		commonkeys.AppVersion, cfg.General.Version,
+	)
+
+	return cfg, nil
 }
